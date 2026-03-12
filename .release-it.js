7a44923a3622e8c3d479b541adcbaf7f7c2eea1f
Heimer <heimer.nguyen@gmail.com>
2026-03-12 16:53:25 +0700

    chore: simplify release-it configuration by changing preset format

--- .release-it.js
+++ .release-it.js
@@ -11,9 +11,7 @@ module.exports = {
   },
   plugins: {
     '@release-it/conventional-changelog': {
-      preset: {
-        name: 'angular',
-      },
+      preset: 'angular',
       infile: 'CHANGELOG.md',
     },
   },
